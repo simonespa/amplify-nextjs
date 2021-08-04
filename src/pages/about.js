c7bddd6de3d3e596115851c6b2e9c9f83fc875ab
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2021-08-04 08:41:44 +0100

    Testing 'getServerSideProps'

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   return {
     props: {
-      description: 'This is the ABOUT page'
+      description: 'What about this page?'
     }
   }
 }
