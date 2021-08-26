63a0f490215809ad145c6037ce100d583a76eb18
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2021-08-26 10:46:46 +0100

    Update SSR page, add custom header

--- src/pages/ssr.js
+++ src/pages/ssr.js
@@ -5,13 +5,13 @@ import Link from 'next/link';
 export async function getServerSideProps() {
   return {
     props: {
-      description: 'What about this page?'
+      description: 'This is a server-side rendered page'
     }
   }
 }
 
 
-export default function SSR() {
+export default function SSR({ description }) {
   return (
     <>
       <Head>
@@ -21,6 +21,7 @@ export default function SSR() {
 
       <main>
         <h1>Server Side Rendering</h1>
+        <p>{description}</p>
 
         <Link href="/">
           <a>
